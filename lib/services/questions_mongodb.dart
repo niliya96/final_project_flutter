859fa0a23094af48a2beb17d7df765cf56b44262
noy israeli <noyisraeli22@gmail.com>
2021-05-13 00:23:33 +0300

    solve db

--- lib/services/questions_mongodb.dart
+++ lib/services/questions_mongodb.dart
@@ -1,12 +1,13 @@
 import 'package:mongo_dart/mongo_dart.dart';
+import 'package:sevr/sevr.dart';
 
-void start() async{
+void start() async {
   final db = await Db.create(
-    'mongodb+srv://muser:amuser@cluster0.f1hwb.mongodb.net/finalProject?retryWrites=true&w=majority');
+      'mongodb+srv://muser:Aa123456@cluster0.f1hwb.mongodb.net/finalProject?retryWrites=true&w=majority');
   await db.open();
-  final coll = db.collection('reviews');
+  final coll = db.collection('questions');
   //var data = <Map<String, dynamic>>[];
   //await coll.insert(data[0], writeConcern: WriteConcern.ACKNOWLEDGED);
-  var response = await coll.find();
+  var response = await coll.find().toList();
   print(response);
 }
