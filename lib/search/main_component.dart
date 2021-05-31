01c23c8ab99472934bd89b1154dc04dba89d73d6
niliya96 <1niliya1@gmail.com>
2021-05-31 21:55:09 +0300

    meeting commit

--- lib/search/main_component.dart
+++ lib/search/main_component.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_firebase/home/home.dart';
 import 'button_search.dart';
 import 'headline.dart';
 import 'input_formats.dart';
@@ -20,69 +21,102 @@ class MainComponentSearchState extends State<MainComponentSearch> {
 
   @override
   Widget build(BuildContext context) {
-      InputFormatsSearch input= new InputFormatsSearch("", "");
+      //InputFormatsSearch input= new InputFormatsSearch("", "");
+      int _currentIndex = 0;
       // main Scaffold 
-      return Scaffold(
-      backgroundColor: const Color(0xff353549),
-      appBar: AppBar(
-        backgroundColor: Colors.lightBlue,
-      ),
-      body:  Stack(
-        children: <Widget>[
-          HeadLineSearch(),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(20,400,20,0),
-            child: Container(child: Column(
-              children: [
-                buildName(),
-                buildPassport(),
-              ],
-            )),
-          ),
-          ButtonSearch(this.widget.nameTyped, this.widget.passportTyped, this.widget.list)
-        ]
-      )
-    );
+      var scaffold = Scaffold(
+            backgroundColor: Colors.white,
+            appBar: AppBar(
+              backgroundColor: Color.fromRGBO(67, 232, 137, 50)
+            ),
+              body:  Stack(
+              children: <Widget>[
+                HeadLineSearch(),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(20,400,20,0),
+                  child: Container(child: Column(
+                    children: [
+                      buildPassport(),
+                    ],
+                  )),
+                ),
+                ButtonSearch(this.widget.nameTyped, this.widget.passportTyped, this.widget.list)
+              ]
+            ),
+            bottomNavigationBar: BottomNavigationBar(
+                backgroundColor: Color.fromRGBO(67, 232, 137, 50),
+                currentIndex: _currentIndex,
+                iconSize: 30,
+                selectedFontSize: 15,
+                unselectedFontSize: 10,
+                items: [
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.home,
+                    color: Color.fromRGBO(0, 48, 80, 50)),
+                    title: Text(
+                      "בית",
+                      style: TextStyle(color: Color.fromRGBO(0, 48, 80, 50)),
+                    ),
+                    backgroundColor: Color.fromRGBO(67, 232, 137, 50),
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.search,
+                    color: Color.fromRGBO(0, 48, 80, 50)),
+                    title: Text(
+                      "חיפוש",
+                      style: TextStyle(color: Color.fromRGBO(0, 48, 80, 50)),
+                    ),
+                    backgroundColor: Color.fromRGBO(67, 232, 137, 50),
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.add,
+                    color: Color.fromRGBO(0, 48, 80, 50)),
+                    title: Text(
+                      'הוספה',
+                      style: TextStyle(color: Color.fromRGBO(0, 48, 80, 50)),
+                    ),
+                    backgroundColor: Color.fromRGBO(67, 232, 137, 50),
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.edit,
+                    color: Color.fromRGBO(0, 48, 80, 50)),
+                      title: Text(
+                      'עדכון',
+                      style: TextStyle(color: Color.fromRGBO(0, 48, 80, 50)),
+                    ),
+                    backgroundColor: Color.fromRGBO(67, 232, 137, 50),
+                  ),
+                ],
+                onTap: (index) {
+                  setState(() {
+                    _currentIndex = index;
+                  });
+                   if (_currentIndex == 0) {
+                    Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context)=> HomeScreen(this.widget.list)));
+                   }   
+                   else if (_currentIndex == 1) {
+                    Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context)=> MainComponentSearch(this.widget.list)));
+                  }
+                  else if (_currentIndex == 2) {
+                    print(2);
+                  }
+                },
+              ),
+            );
+        return scaffold;
   }
   
-  Widget buildName() { 
+  Widget buildPassport() { 
      return TextFormField(
         decoration: InputDecoration(
-          labelText: 'שם מלא',
-          border: OutlineInputBorder(),
-            errorBorder:
-               OutlineInputBorder(borderSide: BorderSide(color: Colors.purple)),
-           focusedErrorBorder:
-               OutlineInputBorder(borderSide: BorderSide(color: Colors.purple)),
-           errorStyle: TextStyle(color: Colors.purple),
-        ),
-        validator: (value) {
-          if (value.length < 0) {
-            return '';
-          } else {
-            return null;
-          }
-        },
-        maxLength: 30,
-        onChanged: (value) => setState(() {
-            name = value;
-            this.widget.nameTyped = value;
-          }
-        ),
-      );
-    }
-
-    Widget buildPassport() { 
-     return TextFormField(
-        decoration: InputDecoration(
+          fillColor: Color.fromRGBO(0, 48, 80, 50),
+          focusColor: Color.fromRGBO(0, 48, 80, 50),
+          hoverColor: Color.fromRGBO(0, 48, 80, 50),
+          filled: true,
           labelText: 'מספר דרכון',
           border: OutlineInputBorder(
+              
           ),
-          errorBorder:
-               OutlineInputBorder(borderSide: BorderSide(color: Colors.purple)),
-          focusedErrorBorder:
-               OutlineInputBorder(borderSide: BorderSide(color: Colors.purple)),
-          errorStyle: TextStyle(color: Colors.purple),
         ),
         validator: (value) {
           if (value.length < 9) {
