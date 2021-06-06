e857f98c70346ea3cb4c18ef12a7a2ba26aa7c0d
niliya96 <1niliya1@gmail.com>
2021-06-06 08:40:03 +0300

    some fixes

--- lib/login/head_line.dart
+++ lib/login/head_line.dart
@@ -2,50 +2,50 @@ import 'package:flutter/material.dart';
 import 'package:adobe_xd/pinned.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-
-
 class HeadLineLogin extends StatelessWidget {
   HeadLineLogin({
     Key key,
   }) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: <Widget>[
-          // text 
-          Container(
-            child: Padding(
-              padding: const EdgeInsets.fromLTRB(75, 40, 75, 40),
-              child: Text(
-                'ExWorkerReviewer',
-                style: TextStyle(
-                  fontFamily: 'Europa',
-                  fontSize: 30,
-                  color: Color.fromRGBO(0, 48, 80, 50),
-                  fontWeight: FontWeight.w700,
-                  height: 1.1666666666666667,
-                ),
-                textHeightBehavior:
-                    TextHeightBehavior(applyHeightToFirstAscent: false),
-                textAlign: TextAlign.center,
-              ),
+    return Stack(children: <Widget>[
+      // text
+      Container(
+        decoration: BoxDecoration(
+          
+          color: const Color(0xff7c94b6),
+          ),
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(75, 40, 75, 40),
+          child: Text(
+            'ExWorkerReviewer',
+            style: TextStyle(
+              fontFamily: 'Europa',
+              fontSize: 30,
+              color: Color.fromRGBO(0, 48, 80, 50),
+              fontWeight: FontWeight.w700,
+              height: 1.1666666666666667,
+            ),
+            textHeightBehavior:
+                TextHeightBehavior(applyHeightToFirstAscent: false),
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ),
+      // image
+      Padding(
+        padding: const EdgeInsets.fromLTRB(0, 90, 0, 100),
+        child: Container(
+          width: 430.0,
+          height: 270.0,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: const AssetImage('assets/images/open1.jfif'),
+              fit: BoxFit.fill,
             ),
           ),
-          // image
-          Padding(
-            padding: const EdgeInsets.fromLTRB(0, 90, 0, 100),
-            child: Container(
-                width: 430.0,
-                height: 270.0,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: const AssetImage('assets/images/open1.jfif'),
-                    fit: BoxFit.fill,
-                  ),
-                ),
-            ),
-          ),
-      ]
-    );
+        ),
+      ),
+    ]);
   }
 }
